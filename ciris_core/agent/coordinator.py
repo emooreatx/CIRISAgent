abb0b1097fac98ff0392d0b6ab59855f917aca3f
Eric Moore <mooreericnyc@gmail.com>
2025-05-26 17:14:36 -0500

    begin refactor into ciris_core

--- ciris_core/agent/coordinator.py
+++ ciris_core/agent/coordinator.py
@@ -5,8 +5,8 @@ receives a :class:`Thought` and returns the recommended
 :class:`HandlerActionType`. Real DMA logic will be integrated later.
 """
 
-from ciris_engine.schemas.agent_core_schemas_v1 import Thought
-from ciris_engine.schemas.foundational_schemas_v1 import HandlerActionType
+from ciris_core.schemas.agent_core_schemas_v1 import Thought
+from ciris_core.schemas.foundational_schemas_v1 import HandlerActionType
 
 
 class Coordinator:
