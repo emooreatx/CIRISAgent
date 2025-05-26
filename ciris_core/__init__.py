795855499275405adb52d9ec6fe755f891729425
Eric Moore <mooreericnyc@gmail.com>
2025-05-26 17:51:38 -0500

    New schemas

--- ciris_core/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Simplified CIRIS runtime core logic."""
-
-from .schemas.states import AgentState
-from .agent.coordinator import Coordinator
-from .agent.processor import Processor
-
-__all__ = ["AgentState", "Coordinator", "Processor"]
