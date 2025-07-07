e1e7513646e221a07ee31d90632ab74cf0ad0222
mainframeai <martin@definition.studio>
2025-07-07 16:20:49 +0200

    Fonts

--- CIRISGUI/apps/agui/app/layout.tsx
+++ CIRISGUI/apps/agui/app/layout.tsx
@@ -1,5 +1,7 @@
 "use client";
-
+import localFont from "next/font/local";
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "../contexts/AuthContext";
 import { Layout } from "../components/Layout";
@@ -7,6 +9,25 @@ import { Toaster } from "react-hot-toast";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState } from "react";
 import { usePathname } from "next/navigation";
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+const fontBrandRegular = localFont({
+  src: "./fonts/AspektaVF.woff2",
+  variable: "--font-brand-regular",
+  display: "swap",
+  style: "normal",
+  weight: "100 900",
+  preload: true,
+  fallback: ["sans-serif"],
+});
 
 export default function RootLayout({
   children,
